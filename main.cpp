08ac68a00137a9e2d5965605145855b19cd9b7fa
FredyR4zox <frederico.almeida@live.com.pt>
2019-05-11 17:03:54 +0100

    Calculate minWorkers if all tasks start at earliest start done

--- main.cpp
+++ main.cpp
@@ -1,16 +1,18 @@
 #include <cstdio>
 #include <vector>
 #include <queue>
+#include <set>
 #include "Task.h"
 
 using namespace std;
 
-int CPM(int nTasks, vector<Task*>& tasks){
-    int ES[nTasks+2], prec[nTasks+2], entStep[nTasks+2], minDur = -1, vf = 0;
+vector<int> CPM(vector<Task*>& tasks, int nTasks){
+    vector<int> ES = vector<int>(nTasks+2, 0);
+    int prec[nTasks+2], entStep[nTasks+2], minDur = -1, vf = 0;
     queue<int> q;
 
     for(int i=0; i<=nTasks+1; i++){
-        ES[i] = 0;
+        //ES[i] = 0;
         prec[i] = -1;
         entStep[i] = 0;
     }
@@ -26,8 +28,8 @@ int CPM(int nTasks, vector<Task*>& tasks){
         int v = q.front();
         q.pop();
 
-        if(minDur < ES[v])
-            minDur=ES[v], vf = v;
+        // if(minDur < ES[v])
+        //     minDur=ES[v], vf = v;
 
         vector<int> children = tasks[v]->getChildren();
         for(int w : children){
@@ -39,8 +41,40 @@ int CPM(int nTasks, vector<Task*>& tasks){
         }
     }
 
-    return minDur;
+    return ES;
 }
+
+int minWorkers(vector<Task*>& tasks, int nTasks, vector<int>& ES){
+    int nWorkers = 0, maxWorkers = 0;
+
+    auto comp = [](const pair<int, int>& a, const pair<int, int>& b) -> bool {
+        // If they have the same time, put first the negatives (end of a task)
+        if(a.first == b.first)
+            return a.second < b.second;
+        return a.first < b.first;
+    };
+    /* This multiset's pair first value will be the start time or end time of a task and
+    the second value will be the number of workers needed or the number of workers
+    not needed anymore (positive/negative integer) */
+    multiset<pair<int, int>, decltype(comp)> time(comp);
+    for(int i=0; i<=nTasks+1; i++){
+        time.insert(make_pair(ES[i], tasks[i]->getNWorkers()));
+        //printf("boas %d\n", tasks[i]->getNWorkers());
+        time.insert(make_pair(ES[i]+tasks[i]->getDuration(), -tasks[i]->getNWorkers()));
+    }
+
+    for(pair<int, int> p : time){
+        //printf("removed %d %d\n", p.first, p.second);
+        nWorkers += p.second;
+        //printf("nWorkers %d\n", nWorkers);
+        if(nWorkers > maxWorkers)
+            maxWorkers = nWorkers;
+    }
+
+    //printf("aaa %d\n", nWorkers);
+    return maxWorkers;
+}
+
 
 
 int main(){
@@ -56,11 +90,11 @@ int main(){
     for(int i=0; i<nTasks; i++){
         scanf("%d %d", &id, &numberOfPrecedings);
         tasks[id]->setID(id);
-        tasks[id]->setNumberOfPrecedings(numberOfPrecedings);
 
         while(numberOfPrecedings--){
             scanf("%d", &preceding);
-            tasks[preceding]->addChildren(id);
+            tasks[id]->addChildren(preceding);
+            tasks[preceding]->setNumberOfPrecedings(tasks[preceding]->getNumberOfPrecedings()+1);
         }
 
         scanf("%d %d\n", &duration, &nWorkers);
@@ -89,7 +123,14 @@ int main(){
     tasks[0] = start;
     tasks[nTasks+1] = end;
 
-    printf("minDur %d\n", CPM(nTasks, tasks));
+    vector<int> ES = CPM(tasks, nTasks);
+//    vector<int> EF = ES;
+//   for(int i=0; i<=nTasks+1; i++)
+//        EF[i] = ES[i] + tasks[i]->getDuration();
+//        printf("ES[%d] = %d\n", i, ES[i]);
+
+    printf("minDur %d\n", ES[nTasks+1]);
+    printf("minWorkers %d\n", minWorkers(tasks, nTasks, ES));
 
     for(Task* t : tasks)
         delete t;
